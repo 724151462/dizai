1c8907dea03fb0041a12d835cd538a0e5274384b
xingkongv <xingcengv@gamil.com>
2020-05-16 09:34:10 +0800

    5.1-5.4个人信息页面

---
+++ src/views/personal/disasterReport.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <personalHead></personalHead>
+    <div
+      class="pu-column al-start pad-tb-5 pad-lr-10 bg-white"
+      style="align-items: flex-start;"
+      v-for="(item,index) in 5"
+      :key="index"
+    >
+      <h3>2020年第3轮降雨(7月1日-9月31日)</h3>
+      <div
+        class="pu-column dz-detail pad-tb-10"
+        style="align-items: flex-start;"
+      >
+        <div class="mar-tb-5">
+          <span class="f-gray">4月1日&nbsp;&nbsp;19:21</span>
+          <span style="margin-left: 200px;"><van-tag plain>已上报</van-tag></span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style>
+  
+</style>
---
+++ src/views/personal/handPic.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <personalHead></personalHead>
+    <div v-for="(item,index) in 3" :key="index" class="pad-lr-10 pad-tb-10 pu-column al-start mar-t-10" :class="index !=6 ? 'border-split' : ''">
+      <span class="f-22"><b>光泽县止马镇止马村15号</b></span>
+      <span class="mar-tb-10">路道开裂</span>
+      <div class="img-wrapper pu-row">
+        <img
+          height="100"
+          width="100"
+          class="mar-r-5"
+          v-for="(pic, index) in 3"
+          src="../../assets/imgs/bad-bg.png"
+          :key="index"
+          :preview="index" preview-text="描述文字"
+          alt=""
+        />
+      </div>
+      <div class="mar-t-10 pu-row">
+        <span class="f-gray">4月13日&nbsp;&nbsp;11:05</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      
+    }
+  },
+}
+</script>
+
+<style scope>
+
+</style>
